48d5aa72c1fdf413a1d3c72a520f9db9c4ed89b6
Diogo Nogueira <diogonogsousa@gmail.com>
2024-09-03 15:19:34 -0300

    :bug: Arruma padrão dos nomes

--- src/api/services/TicketServices.ts
+++ src/api/services/TicketServices.ts
@@ -1,6 +1,15 @@
 import { AppDataSource } from "../../data-source";
-import { Movie } from "../../database/entities/Movie";
+import { Ticket } from "../../database/entities/Ticket";
 
 export class TicketService {
-    
+    private ticketRepository = AppDataSource.getRepository(Ticket)
+
+    async createTicket(movieId:number, sessionId:number):json{
+        const ticket = this.ticketRepository.create(
+            {
+                movieId:movieId, 
+                sessionId:sessionId
+            }
+        )
+    }
 }
