357d7ddb27d60af18f3c246eb6cbc31e46bd15cd
Julyana Mira <Julyanamira@gmail.com>
2024-09-03 20:27:54 -0300

    :bug: fix: Corrige erro em PrimaryGeneratedColumn

--- src/database/entities/Session.ts
+++ src/database/entities/Session.ts
@@ -4,7 +4,7 @@ import { Entity, PrimaryGeneratedColumn, Column, Unique } from 'typeorm';
 @Entity('sessions') // Nome da tabela no banco de dados
 @Unique(['room', 'time'])
 export class Session {
-  @PrimaryGeneratedColumn('uuid') // Campo da chave primária que é gerado automaticamente
+  @PrimaryGeneratedColumn() // Campo da chave primária que é gerado automaticamente
   id!: number;
 
   /*
