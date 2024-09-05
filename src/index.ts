04109ca490481524aabba683d54c2c17fae824d8
claricealvs <claricealves330@gmail.com>
2024-09-05 15:45:09 -0300

    ♻️ refactor: Corrige importações e conexão ao banco de dados

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,33 @@
 import 'reflect-metadata';
 import express from 'express';
+import dotenv from 'dotenv';
+import connect from './database/connection';
 import moviesRoutes from './routes/movieRoutes';
 import sessionRoutes from './routes/sessionRoutes';
 import ticketRoutes from './routes/ticketRoutes';
-import dotenv from 'dotenv';
-import connect from './database/connection';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-// rotas de movies, session e ticket
+// Middleware para rotas
 app.use('/api', moviesRoutes, sessionRoutes, ticketRoutes);
 
-// Iniciar a conexão com o banco de dados e o servidor
-app.listen(3000, async () => {
-  await connect();
-  console.log('Servidor rodando na porta 3000');
-});
+async function startServer() {
+  try {
+    // Iniciar a conexão com o banco de dados
+    await connect();
+    console.log('Conexão com o banco de dados estabelecida com sucesso.');
+
+    // Iniciar o servidor
+    app.listen(3000, () => {
+      console.log('Servidor rodando na porta 3000');
+    });
+  } catch (error) {
+    console.error('Erro ao iniciar o servidor:', error);
+  }
+}
+
+// Chamar a função para iniciar o servidor
+startServer();
