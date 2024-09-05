e711f9cf87a1b1c6a2ee3b3776f5d07a06353fbc
claricealvs <claricealves330@gmail.com>
2024-09-05 15:49:18 -0300

    ✨ feat:  Adiciona funcionalidades as rotas comentadas

--- src/routes/ticketRoutes.ts
+++ src/routes/ticketRoutes.ts
@@ -13,10 +13,19 @@ router.put('/movies/{movie_id}/sessions/{session_id}/tickets/:id , ticketControl
 router.delete('/movies/{movie_id}/sessions/{session_id}/tickets/:id' , ticketController.deleteTicket.bind(ticketController));
 
 */
-//router.post('/movies/:movie_id/sessions/:session_id/tickets', ticketController.createTicket.bind(ticketController))
+router.post(
+  '/movies/:movie_id/sessions/:session_id/tickets',
+  ticketController.createTicket.bind(ticketController),
+);
 
-//router.put('/movies/:movie_id/sessions/:session_id/tickets/:id', ticketController.updateTicket.bind(ticketController))
+router.put(
+  '/movies/:movie_id/sessions/:session_id/tickets/:id',
+  ticketController.updateTicket.bind(ticketController),
+);
 
-//router.delete('/movies/:movie_id/sessions/:session_id/tickets/:id', ticketController.deleteTicket.bind(ticketController))
+router.delete(
+  '/movies/:movie_id/sessions/:session_id/tickets/:id',
+  ticketController.deleteTicket.bind(ticketController),
+);
 
 export default router;
