30da0bc7683734341b54eb80f9ecfe98da0e64d2
claricealvs <claricealves330@gmail.com>
2024-09-05 15:34:27 -0300

    ♻️ refactor: Corrige com Prettier

--- src/api/services/TicketServices.ts
+++ src/api/services/TicketServices.ts
@@ -1,60 +1,59 @@
-import { AppDataSource } from "../../database/data-source";
-import { Ticket } from "../../database/entities/Ticket";
+import { AppDataSource } from '../../database/data-source';
+import { Ticket } from '../../database/entities/Ticket';
 
 export class TicketService {
-    private ticketRepository = AppDataSource.getRepository(Ticket);
+  private ticketRepository = AppDataSource.getRepository(Ticket);
 
-    async createTicket(
-        //movie_id: number,
-        //session_id: number,
-        chair: string,
-        value: number,
-    
-    
-    ): Promise<Ticket>{
-        const ticket = this.ticketRepository.create(
-            {
-                //movie_id, 
-                //session_id,
-                chair,
-                value,
-            }
-        );
+  async createTicket(
+    //movie_id: number,
+    //session_id: number,
+    chair: string,
+    value: number,
+  ): Promise<Ticket> {
+    const ticket = this.ticketRepository.create({
+      //movie_id,
+      //session_id,
+      chair,
+      value,
+    });
 
-        await this.ticketRepository.save(ticket);
+    await this.ticketRepository.save(ticket);
 
-        return ticket;
+    return ticket;
+  }
+
+  async updateTicket(
+    id: string,
+    // movieId: number,
+    // sessionId: number,
+    chair: string,
+    value: number,
+  ): Promise<void> {
+    const existingTicket = await this.ticketRepository.findOne({
+      where: { id },
+    });
+
+    if (!existingTicket) {
+      throw new Error('O ingresso não existe.');
     }
 
-    async updateTicket(
-        id: string,
-        // movieId: number,
-        // sessionId: number,
-        chair: string,
-        value: number,
-        
-        ): Promise<void>{
-            const existingTicket = await this.ticketRepository.findOne({ where: { id } });
+    await this.ticketRepository.update(id, {
+      // movieId,
+      // sessionId,
+      chair,
+      value,
+    });
+  }
 
-            if(!existingTicket){
-                throw new Error('O ingresso não existe.');
-            }
+  async deleteTicket(id: string): Promise<void> {
+    const existingTicket = await this.ticketRepository.findOne({
+      where: { id },
+    });
 
-            await this.ticketRepository.update(id, {
-                // movieId,
-                // sessionId,
-                chair,
-                value,
-            });
-        }
-
-    async deleteTicket(id: string): Promise<void> {
-        const existingTicket = await this.ticketRepository.findOne({ where: { id } });
-
-        if (!existingTicket) {
-            throw new Error('O ingresso não existe.');
-        }
-
-        await this.ticketRepository.delete(id);
+    if (!existingTicket) {
+      throw new Error('O ingresso não existe.');
     }
+
+    await this.ticketRepository.delete(id);
+  }
 }
