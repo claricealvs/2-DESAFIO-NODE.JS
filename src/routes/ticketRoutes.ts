6ee8c0d08f7c834bebb7cf5af8ddf269d9a0d601
Diogo Nogueira <diogonogsousa@gmail.com>
2024-09-05 13:09:41 -0300

    :sparkles: feat: Adiciona a rota para deletar ticket

--- src/routes/ticketRoutes.ts
+++ src/routes/ticketRoutes.ts
@@ -1,10 +1,9 @@
 // src/routes/ticketRoutes.ts
 import { Router } from 'express';
-
-//import { TicketController } from '../api/controllers/TicketController';
+import { TicketController } from '../api/controllers/TicketController';
 
 const router = Router();
-//const ticketController = new TicketController();
+const ticketController = new TicketController();
 
 // Rota para obter todos os filmes
 /*
@@ -14,8 +13,10 @@ router.put('/movies/{movie_id}/sessions/{session_id}/tickets/:id , ticketControl
 router.delete('/movies/{movie_id}/sessions/{session_id}/tickets/:id' , ticketController.deleteTicket.bind(ticketController));
 
 */
-router.post('/movies/:movie_id/sessions/:session_id/tickets', )
+router.post('/movies/:movie_id/sessions/:session_id/tickets', ticketController.createTicket.bind(ticketController))
 
-router.put('/movies/:movie_id/sessions/:session_id/tickets/:id', )
+router.put('/movies/:movie_id/sessions/:session_id/tickets/:id', ticketController.updateTicket.bind(ticketController))
+
+router.delete('/movies/:movie_id/sessions/:session_id/tickets/:id', ticketController.deleteTicket.bind(ticketController))
 
 export default router;
