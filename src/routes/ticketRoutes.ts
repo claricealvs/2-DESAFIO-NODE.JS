8b8350b47719eb0c2141d443e6dee5182113f96b
Diogo Nogueira <diogonogsousa@gmail.com>
2024-09-07 12:36:39 -0300

    :broom: cleanup: retira comentários inúteis

--- src/routes/ticketRoutes.ts
+++ src/routes/ticketRoutes.ts
@@ -5,14 +5,6 @@ import { TicketController } from '../api/controllers/TicketController';
 const router = Router();
 const ticketController = new TicketController();
 
-// Rota para obter todos os filmes
-/*
-
-router.post('/movies/{movie_id}/sessions/{session_id}/tickets' , ticketController.postTicket.bind(ticketController));
-router.put('/movies/{movie_id}/sessions/{session_id}/tickets/:id , ticketController.putTicket.bind(ticketController));
-router.delete('/movies/{movie_id}/sessions/{session_id}/tickets/:id' , ticketController.deleteTicket.bind(ticketController));
-
-*/
 router.post(
   '/movies/:movie_id/sessions/:session_id/tickets',
   ticketController.createTicket.bind(ticketController),
