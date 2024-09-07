4356192372bda028da55d28d20325d12502e1882
Diogo Nogueira <diogonogsousa@gmail.com>
2024-09-07 12:39:30 -0300

    :books: docs: adiciona comentarios organizativos

--- src/routes/ticketRoutes.ts
+++ src/routes/ticketRoutes.ts
@@ -5,16 +5,19 @@ import { TicketController } from '../api/controllers/TicketController';
 const router = Router();
 const ticketController = new TicketController();
 
+//rota post para adicionar novo ticket
 router.post(
   '/movies/:movie_id/sessions/:session_id/tickets',
   ticketController.createTicket.bind(ticketController),
 );
 
+//rota put para atualizar ticket
 router.put(
   '/movies/:movie_id/sessions/:session_id/tickets/:id',
   ticketController.updateTicket.bind(ticketController),
 );
 
+//rota delete para remover ticket
 router.delete(
   '/movies/:movie_id/sessions/:session_id/tickets/:id',
   ticketController.deleteTicket.bind(ticketController),
